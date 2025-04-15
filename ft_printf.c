ebea2a532b59eeb144237caf57585b2d4c263970
Elias Bouali <ebouali@fu-r9-p4.s19.be>
2025-04-15 15:51:43 +0200

    version corrige

--- ft_printf.c
+++ ft_printf.c
@@ -1,36 +1,38 @@
+#include <stdarg.h>
 #include <stdlib.h>
-#include "libft_printf.h"
+#include <unistd.h>
 
-int		ft_printf(const	char *format,...)// le format est une convention et les ... nous permet d'accepte un nombre variable d'arguments
+int	ft_printf(const char *format, ...)
+// le format est une convention et les ... nous permet d'accepte un nombre variable d'arguments
 {
-	va_list args;// args convention pour arguments.
-	int		i;
-	int		count;
+	va_list args; // args convention pour arguments.
+	int i;
+	int count;
 
 	i = 0;
 	count = 0;
-	va_start(args,format);
-	while(format[i])
+	va_start(args, format);
+	while (format[i])
 	{
-		if(format == '%' && format[i + 1])
+		if (format == '%' && format[i + 1])
 		{
 			i++;
-			if(format[i] == 'c')
+			if (format[i] == 'c')
 				count += ft_putchar(va_arg(args, int));
-			else if(format[i] == 's')
-				count += ft_putstr(va_arg(args,char *));
-			else if(format[i] == 'd' || format == 'i')
-				count += ft_putnbr(va_arg(args,int));
-			else if(format[i] == 'u')
-				count += ft_put_unsigned(va_arg(args,unsigned int));
-			else if(format[i] == 'x')
-	      count +=
+			else if (format[i] == 's')
+				count += ft_putstr(va_arg(args, char *));
+			else if (format[i] == 'd' || format == 'i')
+				count += ft_putnbr(va_arg(args, int));
+			else if (format[i] == 'u')
+				count += ft_put_unsigned(va_arg(args, unsigned int));
+			else if (format[i] == 'x' || format[i] == 'X')
+				count += ft_puthex(va_arg(args, unsigned int),
+						format[i] == 'X');
 			else
-				count += whrite(1,&format[i],1);
+				count += whrite(1, &format[i], 1);
 		}
 		i++;
 	}
 	va_end(args);
-	reurn (count);
-
+	reurn(count);
 }
--- ft_putnbr.c
+++ ft_putnbr.c
@@ -1,22 +1,21 @@
 #include <unistd.h>
 
-int ft_putnbr(int n)
+int	ft_putnbr(int n)
 {
-    int     count;
-    char    c;
+	intcount;
+	char c;
 
 	count = 0;
-    if (n == -2147483648)
-        return (write(1, "-2147483648", 11));
-    if (n < 0)
-    {
-        count += write(1, "-", 1);
-        n = -n;
-    }
-    if (n >= 10)
-        count += ft_putnbr(n / 10);
-    c = (n % 10) + '0';
-    count += write(1, &c, 1);
-    return (count);
+	if (n == -2147483648)
+		return (write(1, "-2147483648", 11));
+	if (n < 0)
+	{
+		count += write(1, "-", 1);
+		n = -n;
+	}
+	if (n >= 10)
+		count += ft_putnbr(n / 10);
+	c = (n % 10) + '0';
+	count += write(1, &c, 1);
+	return (count);
 }
-
