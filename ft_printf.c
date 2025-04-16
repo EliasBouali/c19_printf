af90ddce49c2823c65e4e330d55b15f78bcdee66
Elias Bouali <ebouali@fu-r9-p4.s19.be>
2025-04-16 18:32:47 +0200

    version norme

--- ft_printf.c
+++ ft_printf.c
@@ -1,6 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ebouali <ebouali@student.s19.be>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/16 18:31:15 by ebouali           #+#    #+#             */
+/*   Updated: 2025/04/16 18:31:17 by ebouali          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdarg.h>
 #include <stdlib.h>
 #include <unistd.h>
+
+int	ft_count(const char *format, int i, va_list args)
+{
+	int	count;
+	int	result;
+
+	if (format[i] == 'c')
+		result += ft_putchar(va_arg(args, int));
+	else if (format[i] == 's')
+		result += ft_putstr(va_arg(args, char *));
+	else if (format[i] == 'd' || format[i] == 'i')
+		result += ft_putnbr(va_arg(args, int));
+	else if (format[i] == 'u')
+		result += ft_put_unsigned(va_arg(args, unsigned int));
+	else if (format[i] == 'x' || format[i] == 'X')
+		result += ft_puthex(va_arg(args, unsigned int), format[i] == 'X');
+	else if (format[i] == '%')
+		result += ft_putchar('%');
+	else if (format[i] == 'p')
+		result += ft_print_ptr(va_arg(args, uintptr_t));
+	else
+		result += whrite(1, &format[i], 1);
+	if (result == -1)
+		return (-1);
+	else
+		return (count += result);
+}
 
 int	ft_printf(const char *format, ...)
 // le format est une convention et les ... nous permet d'accepte un nombre variable d'arguments
@@ -14,26 +53,14 @@ int	ft_printf(const char *format, ...)
 	va_start(args, format);
 	while (format[i])
 	{
-		if (format[i] == '%' && format[i + 1])
+		if (format[i] == '%' && format[i + 1] && (format[i + 1] == 'c'
+				|| format[i + 1] == 's' || format[i + 1] == 'd' || format[i
+				+ 1] == 'i' || format[i + 1] == 'u' || format[i
+				+ 1] == 'x' format[i + 1] == 'X' format[i + 1] == '%' format[i
+				+ 1] == 'p'))
 		{
 			i++;
-			if (format[i] == 'c')
-				count += ft_putchar(va_arg(args, int));
-			else if (format[i] == 's')
-				count += ft_putstr(va_arg(args, char *));
-			else if (format[i] == 'd' || format[i] == 'i')
-				count += ft_putnbr(va_arg(args, int));
-			else if (format[i] == 'u')
-				count += ft_put_unsigned(va_arg(args, unsigned int));
-			else if (format[i] == 'x' || format[i] == 'X')
-				count += ft_puthex(va_arg(args, unsigned int),
-						format[i] == 'X');
-			else if (format[i] == '%')
-				count += ft_putchar('%');
-			else if (format[i] == 'p')
-			count += 
-			else
-				count += whrite(1, &format[i], 1);
+			count += ft_count(format, i, args);
 		}
 		i++;
 	}
